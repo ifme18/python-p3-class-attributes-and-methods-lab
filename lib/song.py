4299d9201599ac1edfd56617bfd72cc17c0877a4
ken sofware engineer <116180374+ifme18@users.noreply.github.com>
2024-12-16 12:25:11 +0300

    Update song.py

--- lib/song.py
+++ lib/song.py
@@ -1,60 +1,68 @@
 class Song:
 
     count = 0
-    genre=[]
-    artist=[]
-    genre_count={}
-    artist_count={}
-    song_count={}
-   
-    
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
-        self.artist_count()
-        self.add_artist()
-        self.add_genres(genre)
-        self.name= name
-        self.artist=artist
-        self.genre=genre
-        self.genre_count(genre)
-        self.artist_count(artist)
+        
+        self.name = name
+        self.artist = artist
+        self.genre = genre
 
+        
+        Song.add_song_to_count()
 
-
-    
-        pass
-    @classmethod
-    def add_genres(cls,genre):
-        if genre not in cls.genre:
-            cls.genre.append(genre)
-        return cls.genre
-    @classmethod
-    def add_artist(cls,artist):
-        if artist not in cls.artist:
-            cls.artist.append(artist)
-        return cls.artist    
+        
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
 
     @classmethod
-    def artist_count(cls , artist):
-        if artist in cls.artist_count:
-            cls.artist_count[artist]+=1
-        else:
-            cls.artist_count[artist]=1
-        return cls.artist_count
-        #cls.artist_count+=increment
-    
-    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
 
-    def genre_count(cls,genre):
+    @classmethod
+    def add_to_genres(cls, genre):
+        
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # Update genre count dictionary
         if genre in cls.genre_count:
-            cls.genre_count[genre]+=1
+            cls.genre_count[genre] += 1
         else:
-            cls.genre_count[genre]=1
-        return cls.genre_count
+            cls.genre_count[genre] = 1
 
-    @classmethod    
-    def song_count(cls ,increment=1):
-        cls.count+=increment
+    @classmethod
+    def add_to_artist_count(cls, artist):
+    
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+if __name__ == "__main__":
+    
+    song1 = Song("99 Problems", "Jay-Z", "Rap")
+    song2 = Song("Halo", "Beyonce", "Pop")
+    song3 = Song("Hotline Bling", "Drake", "Rap")
+
+    print("Total songs:", Song.count)          
+    print("Artists:", Song.artists)             
+    print("Genres:", Song.genres)               
+    print("Genre count:", Song.genre_count)     
+    print("Artist count:", Song.artist_count)   
     
 
 
